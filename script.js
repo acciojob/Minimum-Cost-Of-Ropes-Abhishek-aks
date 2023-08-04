ea37ea20a7ed51229b3d998b7d0fade83b521e46
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-08-04 20:46:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 function calculateMinCost() {
   //your code here
+	const inputField = document.getElementById("form");
+    const outputElement = document.getElementById("result");
+    const inputValues = inputField.value.split(",");
+    const intArray = inputValues.map(value => parseInt(value.trim(), 10));
+    intArray.sort((a,b) => {return a-b});
+	let cost = 0;
+	let n = intArray.length;
+	while (n > 1) {
+        let arr = intArray.splice(0,2);
+        let sum = arr[0] + arr[1];
+        cost += sum;
+        intArray.push(sum);
+        intArray.sort((a,b) => {return a-b});
+        n = intArray.length;
+	}
+	document.getElementById("result").innerHTML = cost;
   
   
   
